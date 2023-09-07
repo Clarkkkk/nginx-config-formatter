8d89be9e97b0c7b77eca35297a2028078742be79
Aaron_Zhou <clark1729@outlook.com>
2023-09-07 22:58:01 +0800

    chore: adjust vitest config and gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import { nodeExternals } from 'rollup-plugin-node-externals'
+import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         include: ['**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-        outputTruncateLength: 250,
-        outputDiffLines: 30,
         threads: false
     }
 })
