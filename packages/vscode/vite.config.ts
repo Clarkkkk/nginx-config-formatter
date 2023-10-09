98503d5df884ab2e39f7c789e22de89dde9a55ba
Aaron_Zhou <clark1729@outlook.com>
2023-10-09 16:33:54 +0800

    chore(vscode): vite config and vscode launch files

---
+++ packages/vscode/vite.config.ts
@@ -0,0 +1,25 @@
+import { nodeExternals } from 'rollup-plugin-node-externals'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+    build: {
+        target: 'node18',
+        lib: {
+            entry: ['./src/extension.ts'],
+            formats: ['cjs']
+        },
+        rollupOptions: {
+            external: ['vscode']
+        }
+    },
+    plugins: [
+        dts({
+            copyDtsFiles: true
+        }),
+        {
+            ...nodeExternals(),
+            enforce: 'pre'
+        }
+    ]
+})
